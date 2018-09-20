13c3706b547e4bad5185386eca9af4a7be1af39c
Om Mandal <om.mandal@gmail.com>
2018-09-20 13:55:07 -0700

    timelimit.py

--- anaya/timelimit.py
+++ anaya/timelimit.py
@@ -3,15 +3,49 @@
 import subprocess
 import os
 os.environ['PATH'] = '/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin'
+passfile = '/etc/anayapass'
 
 def main():
+    change_password('known')
+
+def main2():
     print_anaya_processes()
     if is_now_a_forbidden_time():
-        change_password_to_secret()
+        change_password('secret')
         kill_anaya_processes()
     else:
-        change_password_to_known()
+        change_password('known')
 
+def is_now_a_forbidden_time():
+    return False
+
+def change_password_to_known():
+    return
+
+def change_password(what):
+    password =  getpass(what)
+    if password == None:
+        print 'Doing nothing'
+        return
+    command = 'dscl . -passwd /Users/anaya'.split()
+    command.append(password)
+    cmdout = subprocess.check_output(command)
+    print cmdout
+
+def getpass(what):
+    if os.path.isfile(passfile):
+        F = open(passfile, 'r')
+        content = F.read()
+        lines = content.splitlines()
+        for line in lines:
+            words = line.split()
+            if words[0] == what:
+                return words[1]
+    else:
+        return False
+
+def kill_anaya_processes():
+    return
 
 def print_anaya_processes():
     cmdout = subprocess.check_output(['ps', 'aux'])
@@ -25,3 +59,5 @@ def print_anaya_processes():
             print words[0]+' '+words[1]+' '+words[10]
 
 main()
+
+
