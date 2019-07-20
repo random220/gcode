f8e15555fee48f975bcf4e65b9980ef852728b02
Om Mandal <om.mandal@gmail.com>
2019-07-20 17:45:06 +0000

    signing-workdir.tbz-aes256-iter1000-HostBang.sh

--- howto/be-your-own-certificate-authority/signing-workdir.tbz-aes256-iter1000-HostBang.sh
+++ howto/be-your-own-certificate-authority/signing-workdir.tbz-aes256-iter1000-HostBang.sh
@@ -16,7 +16,36 @@ else
   exit 1
 fi
 
+if [[ -d 'workdir' ]]; then
+  echo 'WARNING: Directory "workdir" already exists here.'
+  echo
+  echo -n 'OK to delete? [y/n]: '
+  read response
+  if [[ $response == 'y' ]]; then
+    echo -n 'Are you sure? [y/n]: '
+    read response
+    if [[ $response == 'y' ]]; then
+      rm -rf 'workdir'
+    fi
+  fi
+fi
+
+if [[ -d 'workdir' ]]; then
+  echo 'Please manually delete workdir'
+  exit 1
+fi
 
 echo 'Hint: Host!'
 echo
 openssl enc -d -aes256 -iter 1000 <signing-workdir.tbz-aes256-iter1000-HostBang | tar xfj -
+
+
+if [[ ! -d 'workdir' ]]; then
+  echo 'Extraction failed'
+  exit 1
+fi
+
+cd 'workdir'
+git reset --hard
+
+
