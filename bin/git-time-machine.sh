67ce06845ffd67dd0327cf52805b5b7845521807
Om Mandal <omandal@localhost>
2021-06-18 18:02:06 -0700

    git-time-machine.sh

--- bin/git-time-machine.sh
+++ bin/git-time-machine.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
+REPO='/Users/omandal/sb/repos/backup-2021-02-20.git'
 REPO='git-aws:repos/priv-intel-VTd_SIOV_TR--multirel.git'
 
+function init_main() {
+  ensure_data_dirs
+  for REPO in \
+        /Users/omandal/sb/repos/backup-2021-02-25.git \
+        /Users/omandal/sb/repos/backup-2021-03-12.git \
+        /Users/omandal/sb/repos/backup-2021-03-13.git \
+        /Users/omandal/sb/repos/backup-2021-03-31.git \
+        /Users/omandal/sb/repos/backup-2021-04-04.git \
+        /Users/omandal/sb/repos/backup-2021-04-13.git \
+        /Users/omandal/sb/repos/backup-2021-04-18.git \
+        /Users/omandal/sb/repos/backup-2021-05-14.git \
+        /Users/omandal/sb/repos/backup-2021-05-19.git \
+        /Users/omandal/sb/repos/backup-2021-06-02.git \
+        /Users/omandal/sb/repos/backup-2021-06-07.git \
+        /Users/omandal/sb/repos/backup-2021-06-17.git; do
+    (
+      cd data
+      save_refs
+      refetch
+      save_refs
+    )
+  done
+}
+
 function main() {
+  ensure_data_dirs
+  while true; do
+    (
+      cd data
+      save_refs
+      refetch
+      save_refs
+    )
+    sleep 300
+  done
+}
+
+function ensure_data_dirs() {
   if [[ ! -d data ]]; then
     mkdir data
   fi
@@ -25,49 +63,12 @@ function main() {
       git config user.name root
     )
   fi
-
-  while true; do
-    (
-      cd data
-      stashit
-      refetch
-      stashit
-    )
-    sleep 300
-  done
 }
-
 
 function refetch() {
   local NOW=$(date +%s)
   local DATE=$(date)
-  local REMOTE=$(git remote -v | grep fetch|awk '{print $2}')
 
-  if [[ $REMOTE == '' ]]; then
-    REMOTE=$REPO
-    git remote add origin $REMOTE
-  else
-    git remote rm origin
-    git remote add origin $REMOTE
-  fi
-  git fetch --all
-}
-
-function stashit() {
-  local NOW=$(date +%s)
-  local DATE=$(date)
-  local REMOTE==$(git remote -v | grep fetch|awk '{print $2}')
-
-  local r=$(git rev-list --all -1)
-  echo $r >.git/HEAD
-  rsync -a --delete .git/refs/ ../metadata/refs/
-  (
-    cd ../metadata
-    rm -rf refs/heads
-    git add -Af
-    git commit -m "$NOW $DATE REMOTE: $REMOTE"
-  )
-  
   local TIPS=$(git rev-list --branches --remotes --children --tags|grep -v ' ')
   local BRANCHES=$(git branch|grep -v '\*')
   local TAGS=$(git tag)
@@ -88,8 +89,26 @@ function stashit() {
   if [[ $TAGS != '' ]]; then
     git tag -d $TAGS
   fi
+
+  # Now refetch
+  git remote rm origin
+  git remote add origin $REPO
+  git fetch --all
 }
 
+function save_refs() {
+  local NOW=$(date +%s)
+  local DATE=$(date)
+  local REMOTE=$(git remote -v | grep fetch|awk '{print $2}')
+
+  rsync -a --delete .git/refs/ ../metadata/refs/
+  (
+    cd ../metadata
+    rm -rf refs/heads
+    git add -Af
+    git commit -m "$NOW $DATE REMOTE: $REMOTE"
+  )
+}
 
 main
 
