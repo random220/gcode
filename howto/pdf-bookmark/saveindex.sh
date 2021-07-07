25d3da3bf6c192cbe1f5a98048a198e11eb021e3
Om Mandal <omandal@localhost>
2021-07-07 09:55:54 -0700

    saveindex.sh

--- howto/pdf-bookmark/saveindex.sh
+++ howto/pdf-bookmark/saveindex.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo '#!/bin/bash' >.build/a.txt
 echo 'cat <<EOF|base64 -d|gunzip - >rawindex.txt' >>.build/a.txt
-cat index-raw.txt | gzip -9 -|base64 -w 120 >>.build/a.txt
+u=$(uname)
+if [[ $u == Darwin ]]; then
+    cat index-raw.txt | gzip -9 -|base64 -b 120 >>.build/a.txt
+else
+    cat index-raw.txt | gzip -9 -|base64 -w 120 >>.build/a.txt
+fi
 echo 'EOF' >>.build/a.txt
 echo >>.build/a.txt
 mv .build/a.txt .build/index-raw-base64.txt
