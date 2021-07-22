ddbd4e66f4f22477c13e2b99c0a6769540206d71
Om Mandal <omandal@localhost>
2021-07-22 13:34:19 -0700

    server.js

--- howto/webs/server.js
+++ howto/webs/server.js
@@ -5,12 +5,35 @@ npm init -f -y
 npm install ws
 */
 const WebSocket = require('ws')
-
 const wss = new WebSocket.Server({ port: 5678 })
 
+/*
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
   })
-  ws.send('ho!')
+  ws.send(`ho!`)
 })
+*/
+
+/*
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send(`ho!`)
+})
+*/
+
+
+
+wss.on('connection', onconn)
+
+function onconn(ws) {
+  ws.on('message', onmsg)
+  ws.send(`ho!`)
+}
+
+function onmsg(messge) {
+  console.log(`Received message => ${message}`)
+}
