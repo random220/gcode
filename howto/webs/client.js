27fbcca0fb1a5ef0a24e71e920669138e763dd7a
Om Mandal <omandal@localhost>
2021-07-23 08:46:01 +0000

    client.js also works with server.py

--- howto/webs/client.js
+++ howto/webs/client.js
@@ -14,9 +14,38 @@ var connection  = new WebSocket("ws://localhost:5678/")
 //or, equivalently
 //connection.onmessage = (e) => {
 
-connection.onconnect = function(e) {
+connection.onopen = function(e) {
   connection.send('hi')
+  setTimeout(() => {
+    connection.send('ho')
+  }, 2000);
 }
 connection.onmessage = function(e) {
   console.log(e.data)
+  connection.close(1000, "Work complete");
+  /*
+  setTimeout(() => {
+    connection.close(1000, "Work complete");
+  }, 2000);
+  */
 }
+/*
+Most common code values:
+
+1000 - the default, normal closure (used if no code supplied),
+1006 - no way to set such code manually, indicates that the connection was lost (no close frame).
+There are other codes like:
+
+1001 - the party is going away, e.g. server is shutting down, or a browser leaves the page,
+1009 - the message is too big to process,
+1011 - unexpected error on server,
+…and so on.
+The full list can be found in RFC6455, §7.4.1.
+*/
+
+connection.onclose = function() {
+  // websocket is closed.
+}
+
+
+
