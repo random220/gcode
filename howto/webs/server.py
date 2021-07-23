0d0132c9d929c6e44e4d8c6d1965f9015b54968d
Om Mandal <omandal@localhost>
2021-07-23 10:12:49 +0000

    server.py can serve data/omandal and data/karthikrajr files

--- howto/webs/server.py
+++ howto/webs/server.py
@@ -6,11 +6,24 @@ import asyncio
 import datetime
 import random
 import websockets
+import re
+import os
 
 async def lcfc(websocket, path):
     async for message in websocket:
-        await websocket.send(f'got "{message}"')
-        #await websocket.send(f'got "milk"')
+        m = re.search(r'^user\n', message)
+        if m:
+            m = re.search(r'\n(.+?)\n?$', message)
+            if m:
+                user = m.group(1)
+                datafile = f"data/{user}"
+                alltext='anyany'
+                if os.path.isfile(datafile):
+                    with open(datafile, 'rt') as f:
+                        alltext = f.read()
+                await websocket.send(f'{user}\n{alltext}')
+            else:
+                await websocket.send(f'got "{message}"')
 
 server = websockets.serve(lcfc, "0.0.0.0", 5678)
 
