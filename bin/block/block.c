cae2a4a60a4415e888b21242ec5a071a26c8f6f9
Om Mandal <omandal@localhost>
2021-12-25 18:42:09 -0800

    block.c

--- bin/block/block.c
+++ bin/block/block.c
@@ -7,17 +7,17 @@ int main(int argc, char** argv) {
     if (strncmp(argv[1], "open", 5) == 0) {
       setuid(0);
       setgid(0);
-      system("/bin/bash -x /Users/omandal/gcode/bin/block.sh open");
+      system("/bin/bash -x /var/root/bin/block.sh open");
     }
     else if (strncmp(argv[1], "close", 6) == 0) {
       setuid(0);
       setgid(0);
-      system("/bin/bash -x /Users/omandal/gcode/bin/block.sh close");
+      system("/bin/bash -x /var/root/bin/block.sh close");
     }
     else if (strncmp(argv[1], "check", 6) == 0) {
       setuid(0);
       setgid(0);
-      system("/bin/bash -x /Users/omandal/gcode/bin/block.sh check");
+      system("/bin/bash -x /var/root/bin/block.sh check");
     }
   }
   return 0;
