072b1899a88e04c41b0138510a6c1ce50987f7ce
Om Mandal <omandal@localhost>
2023-01-21 09:25:50 -0800

    show a.txt

--- howto/flask/one.py
+++ howto/flask/one.py
@@ -1,10 +1,15 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def display_file():
+    with open("/Users/om/a.txt") as file:
+        contents = file.read()
+    #return "<pre>" + escape(contents) + "</pre>"
+    return "<pre>" + contents + "</pre>"
 
 if __name__ == "__main__":
     app.run()
