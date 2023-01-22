b7e2934eccaeaa380aabbfda9975bae779cca817
Om Mandal <omandal@localhost>
2023-01-22 11:04:00 -0800

    howto/flask/one.py

--- howto/flask/one.py
+++ howto/flask/one.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 from flask import Flask, escape
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
-def display_file():
-    with open("/Users/om/a.txt") as file:
+def display_om():
+    with open(sys.argv[0]) as file:
         contents = file.read()
-    #return "<pre>" + escape(contents) + "</pre>"
-    return "<pre>" + contents + "</pre>"
+    contents = str(escape(contents))
+    return f"<pre>{contents}</pre>"
 
 if __name__ == "__main__":
     app.run()
+
