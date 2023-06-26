1891ef84dcd0a75ab00cb58d35b595ec1247770f
Om Mandal <omandal@localhost>
2023-06-26 02:21:28 +0000

    sshkey

--- howto/aws-terraform/tute/main.tf
+++ howto/aws-terraform/tute/main.tf
@@ -3,9 +3,15 @@ provider "aws" {
     region = "us-east-2"
 }
 
+resource "aws_key_pair" "omkey" {
+  key_name   = "sesame"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "gaia" {
     ami = "ami-024e6efaf93d85776"
     instance_type = "t2.micro"
+    key_name = aws_key_pair.omkey.key_name
 
     tags = {
         Name = "gaia1"
