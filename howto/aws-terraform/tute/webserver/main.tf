4c19aa11e01045a677f32ff5e5091c52c93e04d0
Om Mandal <omandal@localhost>
2023-06-28 23:42:15 +0000

    simple server with two ingress ports open on new security group

--- howto/aws-terraform/tute/webserver/main.tf
+++ howto/aws-terraform/tute/webserver/main.tf
@@ -3,17 +3,40 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_key_pair" "omkey" {
+resource "aws_key_pair" "omkey_sesame" {
   key_name   = "sesame"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/id_rsa-sesame.pub")
+}
+
+resource "aws_security_group" "port_8080" {
+    name = "name_portia_8080"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_instance" "gaia" {
     ami = "ami-024e6efaf93d85776"
     instance_type = "t2.micro"
-    key_name = aws_key_pair.omkey.key_name
+    vpc_security_group_ids = [aws_security_group.port_8080.id]
+    key_name = aws_key_pair.omkey_sesame.key_name
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" >index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
     tags = {
         Name = "gaia1"
     }
 }
+
