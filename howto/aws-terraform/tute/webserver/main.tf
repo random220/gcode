de3a4657a93263ec4503d144398043a42d58bc55
Om Mandal <omandal@localhost>
2023-06-29 05:06:42 +0000

    check

--- howto/aws-terraform/tute/webserver/main.tf
+++ howto/aws-terraform/tute/webserver/main.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "omkey_sesame" {
   public_key = file("~/.ssh/id_rsa-sesame.pub")
 }
 
-resource "aws_security_group" "port_8080" {
+resource "aws_security_group" "port_8080_22" {
     name = "name_portia_8080"
     ingress {
         from_port = 8080
@@ -27,7 +27,7 @@ resource "aws_security_group" "port_8080" {
 resource "aws_instance" "gaia" {
     ami = "ami-024e6efaf93d85776"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.port_8080.id]
+    vpc_security_group_ids = [aws_security_group.port_8080_22.id]
     key_name = aws_key_pair.omkey_sesame.key_name
 
     user_data = <<-EOF
