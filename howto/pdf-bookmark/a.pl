9fe3c74b69ca5f59c9e6f672bb19969df38567e7
Om Mandal <omandal@localhost>
2023-12-28 17:45:44 -0800

    a.pl

---
+++ howto/pdf-bookmark/a.pl
@@ -0,0 +1,13 @@
+while ($l = <>) {
+    chomp $l;
+    if ($l =~ m{^(.*)\s(\d+)\s*$}) {
+        ($x, $y) = ($1, $2);
+        if ($y > 175) {
+            $y += 4;
+        }
+        print "$x $y\n";
+    }
+    else {
+        print "$l\n";
+    }
+}
