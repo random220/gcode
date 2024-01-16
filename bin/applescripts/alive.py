f9fc1fee6dbb42c123cdedf05c4f385057794aca
Om Mandal <omandal@localhost>
2024-01-16 16:18:36 -0500

    alive.py

--- bin/applescripts/alive.py
+++ bin/applescripts/alive.py
@@ -3,6 +3,7 @@
 import os, sys
 import random
 import time
+import subprocess as sp
 
 X = 1728
 Y = 1117
@@ -10,11 +11,16 @@ Y = 1117
 if os.path.isfile('/tmp/.die.alive'):
     os.unlink('/tmp/.die.alive')
 
+p0 = sp.getoutput('cliclick p:.')
+
 while True:
     if os.path.isfile('/tmp/.die.alive'):
         os.unlink('/tmp/.die.alive')
         sys.exit(0)
-    x = random.randint(0, X)
-    y = random.randint(0, Y)
-    os.system(f'cliclick -r -e 1000 m:{x},{y}')
-    time.sleep(8)
+    p1 = sp.getoutput('cliclick p:.')
+    if p1 == p0:
+        x = random.randint(0, X)
+        y = random.randint(0, Y)
+        sp.run(f'cliclick -r -e 50 m:{x},{y}', shell=True)
+    p0 = sp.getoutput('cliclick p:.')
+    time.sleep(60)
