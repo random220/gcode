c5375a32c27f8c3e331718f6a7db7fb41f2a72f1
Om Mandal <omandal@localhost>
2025-03-07 16:07:11 -0800

    songs.pl

--- bin/songs.pl
+++ bin/songs.pl
@@ -1,36 +1,80 @@
 $n = 0;
 while ($l = <DATA>) {
     chomp $l;
-    #$l =~ m{^(.+?)[\s\-]+(\d+)\:(\d+)$};
-    ($h, $m, $s, $title) = $l =~ m{^(\d+)\:(\d+)\:(\d+)\s+(.+)$};
-    $x = $h * 60 * 60 + 60 * $m + $s;
-    $x = $x.'.00000';
-    $n++;
-    print "$x\t$x\t$n - $title\n";
+    if ( $l =~ m{^(\d+)\:(\d+)\:(\d+)\s+(.+)$}) {
+        ($h, $m, $s, $title) = ($1, $2, $3, $4);
+        $x = $h * 60 * 60 + 60 * $m + $s;
+        $x = $x.'.00000';
+        $n++;
+        print "$x\t$x\t${n}_$title\n";
+    }
 }
 __DATA__
-00:00:06 O Mere Dil Ke Chain - Kishore Kumar - R.D. Burman - Majrooh Sultanpuri
-00:04:42 Lag Ja Gale Se Phir - Lata Mangeshkar - Madan Mohan - Raja Mehdi Ali Khan
-00:09:00 Tere Bina Zindagi Se - Lata Mangeshkar, Kishore Kumar - R.D. Burman - Gulzar
-00:14:54 Chura Liya Hai Tumne Jo Dil Ko - Asha Bhosle, Mohammed Rafi - R.D. Burman - Majrooh Sultanpuri
-00:19:42 Bheegi Bheegi Raaton Mein - Lata Mangeshkar, Kishore Kumar - R.D. Burman - Anand Bakshi
-00:23:31 Ek Ajnabee Haseena Se - Kishore Kumar - R.D. Burman - Anand Bakshi
-00:27:57 Tum Aa Gaye Ho Noor Aa Gaya - Lata Mangeshkar, Kishore Kumar - R.D. Burman - Gulzar
-00:32:09 Kora Kagaz Tha Yeh Man Mera - Lata Mangeshkar, Kishore Kumar - S.D. Burman - Anand Bakshi
-00:37:48 Likhe Jo Khat Tujhe - Mohammed Rafi - Shankar_Jaikishan - Neeraj
-00:42:21 Kya Hua Tera Vada - Mohammed Rafi, Sushma Shrestha - R.D. Burman - Majrooh Sultanpuri
-00:46:39 Aap Ki Ankhon Mein Kuch - Kishore Kumar, Lata Mangeshkar - R.D. Burman - Gulzar
-00:50:48 Yeh Sham Mastani - Kishore Kumar - R.D. Burman - Anand Bakshi
-00:55:25 Ajib Dastan Hai Yeh - Lata Mangeshkar - Shankar_Jaikishan - Shailendra
-01:00:41 Mere Sapnon Ki Rani - Kishore Kumar - S.D. Burman - Anand Bakshi
-01:05:41 Abhi Na Jao Chhod Kar - Asha Bhosle, Mohammed Rafi - Jaidev - Sahir Ludhianvi
-01:09:56 Bahon Mein Chale Aao - Lata Mangeshkar - R.D. Burman - Majrooh Sultanpuri
-01:13:58 Hamen Tumse Pyar Kitna - Kishore Kumar - R.D. Burman - Majrooh Sultanpuri
-01:17:59 Bade Achhe Lagte Hain - Amit Kumar - R.D. Burman - Anand Bakshi
-01:23:11 Mere Mehboob Qayamat Hogi - Kishore Kumar - Laxmikant_Pyarelal - Anand Bakshi
-01:27:00 Roop Tera Mastana - Kishore Kumar - S.D. Burman - Anand Bakshi
-01:30:45 Goom Hai Kisi Ke Pyar Mein - Kishore Kumar, Lata Mangeshkar - R.D. Burman - Majrooh Sultanpuri
-01:35:00 Aanewala Pal Janewala Hai - Kishore Kumar - R.D. Burman - Gulzar
-01:39:39 Gaata Rahe Mera Dil - Lata Mangeshkar, Kishore Kumar - S.D. Burman - Shailendra
-01:44:32 Kisi Ki Muskurahaton Pe - Mukesh - Shankar_Jaikishan - Shailendra
-01:49:02 Pyar Hua Iqrar Hua
+https://www.youtube.com/watch?v=4YnSYSu_eU4
+
+This jukebox presents 50 selected Bengali hits from the decade 70's rendered by various eminent artistes like Kishore Kumar, Manna Dey, Lata Mangeshkar & R D Burman,Asha Bhosle,Bhupen Hazarika etc:These songs are very popular to all listeners: Listen, Enjoy And Share It: 
+
+00:00:06 Amar Swapna Tumi Ogo_Kishore Kumar,Asha Bhosle
+00:04:00 Gunjane Dole Je Bhramar_Kishore Kumar,Asha Bhosle
+00:08:16 Asha Chhilo Bhalobasa Chhilo_Kishore Kumar
+00:12:26 Ki Ashai Bandhi Khelaghar_Kishore Kumar
+00:16:53 Prithibi Bodle Gechhe_Kishore Kumar
+00:20:31 Jodi Hoi Chorkanta_Kishore Kumar,Asha Bhosle
+00:23:50 Ek Baishakhe Dekha Holo Dujanay_Arati Mukherjee
+00:27:22 Shaon Raate Jodi_Manna Dey
+00:31:34 Bujhbe Na Keu Bujhbe Na_Lata Mangeshkar
+00:35:42 Ogo Nirupama_Kishore Kumar
+00:38:52 Nayan Sarasi Keno Bhoreche Jaale_Kishore Kumar
+00:42:13 Aamar Bhalobasar Rajprasade_Manna Dey
+00:45:47 Jakhan Keu Amake Pagol Bale_Manna Dey
+00:52:21 Shono Mon Boli Tomay_R:D:Burman
+00:55:31 Tare Ami Chokhe Dekhini_Kishore Kumar
+00:58:35 Amar Deep Nebhano Raat_Kishore Kumar
+01:02:01 Balo Ki Achhe Go_R:D:Burman
+01:05:15 Ami Nei, Ami Nei_Kishore Kumar
+01:08:34 Mohuay Jomechhe Aaj Mou Go_Asha Bhosle
+01:12:06 Sedino Akashe Chhilo Kato Tara_Kishore Kumar
+01:15:28 Ami Kon Pathe Je Chali_Manna Dey
+01:18:42 E Shudhu Gaaner Din_Sandhya Mukherjee
+01:22:12 Mon Metechhe Mon Mayurir_Asha Bhosle
+01:25:24 Behag Jadi Na Hoi_Manna Dey
+01:29:35 Jani Na Aaj Je Apon_Asha Bhosle
+01:32:17 Dekhuk Para Porshite_Shyamal Mitra
+01:35:04 Aaj Hridaye Bhalobese_Kishore Kumar,Lata Mangeshkar
+01:40:40 Sagar Dake Aay Aay_Asha Bhosle
+01:44:17 Ja Ja Ja Bhule Ja_Lata Mangeshkar
+01:47:42 Mor Swapneri Saathi_Kishore Kumar
+01:52:12 Jete Jete Pathe Holo_R:D:Burman
+01:55:19 O Chand Samle Rakho Jochhnake_Manna Dey
+01:58:28 Ke Tumi Tandraharani_Manna Dey
+02:02:30 Sundari Go Dohai Dohai_Manna Dey
+02:05:47 Amar Moner Ei Mayurmahale_Kishore Kumar
+02:08:54 Ami Ek Jajabar_Bhupen Hazarika
+02:11:59 Ei Je Nadi Jay Sagare_Kishore Kumar
+02:15:26 Ka Phonta Chokher Jal Phelechho_Manna Dey
+02:18:50 Takhon Tomar Ekush Bachhar_Arati Mukherjee
+02:22:11 Gahan Megher Chhaya Ghanay_Manna Dey
+02:25:36 Emon Ekti Jhinuk Khunje_Nirmala Mishra
+02:29:12 Hey Dola Hey Dola_Bhupen Hazarika
+02:31:36 Se Amar Chhoto Bon_Manna Dey
+02:38:15 Manush Manusher Jannya_Bhupen Hazarika
+02:41:16 Abhimane Chole Jeo Na_Manna Dey
+02:44:31 Chalo Jai Chole Jai_Kishore Kumar
+02:47:26 Aaj Jiban Khunje Pabi_Bhupen Hazarika
+02:50:13 Madhumas Jay_RD Burman
+02:53:39 Tumi Nijer Mukhei Bolle Jedin_Manna Dey
+02:56:51 Jodi Kagoje Lekho Naam_Manna Dey
+
+
+Label :: Saregama India Ltd
+
+For more videos log on & subscribe to our channel :
+
+   / saregamabengali  
+
+Facebook ::   / saregamabangla  
+
+Twitter ::   / saregamaglobal  
+
+Google+ :: https://plus.google.com/+saregamabengali
+Key moments
